84154adb72fc3a75cf5a452dc9551879b07cdb5a
doom-man <2840072208@qq.com>
2019-12-25 01:56:06 -0800

    doom

--- lua/unknown/lua/unknown/a.lua
+++
@@ -1,1 +0,0 @@
-1asdasdasdaszxczxcsdzxczxczc23
--- lua/unknown/lua/unknown/app/data/segmentData.lua
+++
@@ -1,9 +0,0 @@
---[Comment]
---jnmo
-segmentData = class("segmentData")
-segmentData.__index = segmentData
-function segmentData:ctor(id,state)    
-    self.id = id
-    self.state = state or 0 --0是未连通 1连通     
-end
-
